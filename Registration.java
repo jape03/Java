734835d47d073943a98b844ee4cc0b5e9301a773
jape03 <168762851+jape03@users.noreply.github.com>
2024-09-18 16:29:10 +0800

    Add files via upload

--- Registration.java
+++ Registration.java
@@ -3,11 +3,27 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Registration implements ActionListener{
+public class Registration {
 
     JFrame frameRegistration;
-    JTextField fieldDisplayName;
+    JTextField fieldDisplayName,fieldDisplayAddress,fieldDisplayEmail,fieldDisplayContact;
     JComboBox fieldCourse,fieldYear;
     JRadioButton fieldGender;
+    JButton buttonRegister, buttonClear, buttonExit;
 
+    public Registration(){
+        initializeFrame();
+    }
+
+    public void initializeFrame() {
+        frameRegistration = new JFrame("Registration");
+        frameRegistration.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameRegistration.setSize(400, 550);
+        frameRegistration.setLayout(null);
+       
+    }
+
+    public static void main(String[] args) {
+        new Registration();
+    }
 }
