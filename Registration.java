a88ea3ddef57ca48f1202d954f77a8745f0b272f
jape03 <168762851+jape03@users.noreply.github.com>
2024-09-18 16:14:45 +0800

    Add files via upload

---
+++ Registration.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Registration implements ActionListener{
+
+    JFrame frameRegistration;
+    JTextField fieldDisplayName;
+    JComboBox fieldCourse,fieldYear;
+    JRadioButton fieldGender;
+
+}
